759b01a15c54723834b007981ee5db1a7ebe48dd
gravitacia <kocagg@mail.ru>
2023-12-18 12:59:50 +0300

    feat : add ws functions

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
 import sys
+import threading
+import time
+
+
+def on_message(ws, message):
+    print(f"Received message: {message}")
+
+
+def on_error(ws, error):
+    print(f"Error: {error}")
+
+
+def on_close(ws, close_status_code, close_msg):
+    print(f"Closed with status code {close_status_code}: {close_msg}")
+
+
+def on_open(ws):
+    def run(*args):
+        for i in range(5):
+            message = f"Hello from Thread {threading.current_thread().name} - {i}"
+            ws.send(message)
+            time.sleep(1)
+        ws.close()
+
+    threading.Thread(target=run).start()
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
